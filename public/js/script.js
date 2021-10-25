5fda5d56f42fd50d2f47cf17ee3c0bcc7788542c
nlukic97 <nlukic97@gmail.com>
2021-10-25 14:16:09 +0200

    all controlls are functional

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,10 @@
+
+let videoContainer = document.querySelector('.player')
 let video = document.querySelector('video')
+let controls = document.querySelector('.controls')
+
+video.removeAttribute('controls');
+controls.style.visibility = 'visible';
 
 let currVolume
 
@@ -25,13 +31,11 @@ document.querySelector('button.mute').addEventListener('click',function(){
         console.log(currVolume);
         video.volume = 0;
     }
-
     video.classList.toggle('muted')
 })
 
 
 //video speed playback listeners
-
 let speeds = [
     {
         identifyer:'050',
@@ -61,7 +65,22 @@ speeds.forEach(speed=>{
     })
 })
 
+// fullscreen button
 document.querySelector('button.fullscreen').addEventListener('click',function(){
-    video.requestFullscreen()
+    if(document.fullscreenElement){
+        document.exitFullscreen()
+    } else {
+        openFullScreen(videoContainer)
+    }
 })
+
+function openFullScreen(item){
+    if (item.requestFullscreen) {
+        item.requestFullscreen();
+    } else if (item.webkitRequestFullscreen) { /* Safari */
+        item.webkitRequestFullscreen();
+    } else if (item.msRequestFullscreen) { /* IE11 */
+        item.msRequestFullscreen();
+    }
+}
 
