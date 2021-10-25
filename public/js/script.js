022d0207afc7415edbd352de0597d5e5038a9f42
nlukic97 <nlukic97@gmail.com>
2021-10-25 13:19:47 +0200

    added the controls except volume

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,67 @@
 let video = document.querySelector('video')
-video.play()
+
+let currVolume
+
+
+// Playing and pausing the video
+document.querySelector('button.play-pause').addEventListener('click',function(){
+    console.log(this);
+    
+    if(this.classList.contains('playing')){
+        video.pause()
+    } else {
+        video.play()
+    }
+    
+    this.classList.toggle('playing')
+})
+
+
+document.querySelector('button.mute').addEventListener('click',function(){
+    if(video.classList.contains('muted')){
+        video.volume = currVolume
+    } else {
+        currVolume = video.volume
+        console.log(currVolume);
+        video.volume = 0;
+    }
+
+    video.classList.toggle('muted')
+})
+
+
+//video speed playback listeners
+
+let speeds = [
+    {
+        identifyer:'050',
+        speed:0.5
+    },
+    {
+        identifyer:'075',
+        speed:0.75
+    },
+    {
+        identifyer:'100',
+        speed:1.0
+    },
+    {
+        identifyer:'150',
+        speed:1.5
+    },
+    {
+        identifyer:'200',
+        speed:2
+    },
+]
+
+speeds.forEach(speed=>{
+    document.querySelector(`button.speed-${speed.identifyer}`).addEventListener('click',function(){
+        video.playbackRate = speed.speed
+    })
+})
+
+document.querySelector('button.fullscreen').addEventListener('click',function(){
+    video.requestFullscreen()
+})
+
